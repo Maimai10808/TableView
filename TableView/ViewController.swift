82af0e7d9362aa524c084a50db433f52e140ae69
Maimai10808 <331686549@qq.com>
2025-06-07 16:25:17 +0800

    TableView

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var shoppingItems: [String] = ["Apples", "Bananas", "Oranges"]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingCell", for: indexPath)
+        
+        let shoppingItem = shoppingItems[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = shoppingItem
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         
+    }
 }
 
